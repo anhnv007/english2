27c35534135a6e07f3b2743aa5a6d8dd676ce2ce
Vanh <anhnv@techfox.io>
2020-09-15 09:08:53 +0700

    update module

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
-const express = require("express");
 const path = require("path");
-const port = process.env.PORT || 8080;
+const express = require("express");
 const app = express();
-
-// the __dirname is the current directory from where the script is running
-app.use(express.static(__dirname));
-
-// send the user to index html page inspite of the url
+const publicPath = path.join(__dirname, "..", "english2", "public");
+const port = process.env.PORT || 8080;
+app.use(express.static(publicPath));
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "index.html"));
+  res.sendFile(path.join(publicPath, "index.html"));
 });
-
-app.listen(port);
+app.listen(port, () => {
+  console.log("Server is up!");
+});
